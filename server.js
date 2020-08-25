adee03272074424fe0b49dda9e8a383b549e4d65
Matthew Wong <matthew.raymond.wong@gmail.com>
2020-08-25 15:07:51 +0700

    add vary origin to http header

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 	res.header("Access-Control-Allow-Credentials", true)
+	res.header("Vary", "Origin")
 	next();	
   }); //set response headers
   
